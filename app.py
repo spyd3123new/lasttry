61637d80f14621dcb70a58c09abba31e33defdb1
spyd3123new <spyd3123@gmail.com>
2024-05-05 01:56:48 +0530

    new

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify , render_template
 from runprediction import predictionsz
-from flask_cors import CORS ,cross_origin
 
-app = Flask(__name__, static_folder='my-app/build', static_url_path='')
-CORS(app)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/api', methods=['POST'])
-@cross_origin()
 def api():
     data = request.json
     content = data.get('content')
     recommended_asans = predictionsz(content)
     print("Received content:", content)
     return jsonify({"recommended_asans": recommended_asans})
-
-@app.route('/')
-@cross_origin()
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
